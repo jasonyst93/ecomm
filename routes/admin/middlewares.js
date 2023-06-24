1f1e897ab40054a95a140b51efa941226a8cfbf8
Jason Tam <jasonyst93@gmail.com>
2023-06-24 10:38:37 +0800

    working on shopping cart

--- routes/admin/middlewares.js
+++ routes/admin/middlewares.js
@@ -1,12 +1,17 @@
 const { validationResult } = require('express-validator');
 
 module.exports = {
-  handleErrors(templateFunc) {
-    return (req, res, next) => {
+  handleErrors(templateFunc, dataCb) {
+    return async (req, res, next) => {
       const errors = validationResult(req);
 
       if (!errors.isEmpty()) {
-        return res.send(templateFunc({ errors }));
+        let data = {};
+        if (dataCb) {
+          data = await dataCb(req); //call the function if callback exist
+        }
+
+        return res.send(templateFunc({ errors, ...data })); //pass in data if it isn't undefined
       }
 
       next();
